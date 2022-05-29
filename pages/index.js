aaf6c3e156da5fb2254927f8c6f6651d8ced2867
vihangatheturtle <43118457+vihangatheturtle@users.noreply.github.com>
2022-05-29 21:26:09 +0100

    check if on mobile

--- pages/index.js
+++ pages/index.js
@@ -4,8 +4,11 @@ import styles from '../styles/Home.module.css'
 import { useState, useRef, useEffect } from 'react'
 
 export default function Home() {
+  const [isMobile, setIsMobile] = useState(false);
+
   const outputBox = useRef(null);
   const inBox = useRef(null);
+  const mobileWarning = useRef(null);
 
   const help = [
     '<b>about</b>      about me',
@@ -37,27 +40,47 @@ export default function Home() {
   }
 
   useEffect(() => {
-    document.getElementsByClassName("input")[0].style.display = "block";
-    inBox.current.focus();
-    inBox.current.addEventListener('blur', (e) => {
+    let isInMobile = Boolean(navigator.userAgent.match(
+      /Android|BlackBerry|iPhone|iPad|iPod|Opera Mini|IEMobile|WPDesktop/i
+    ))
+    if (!isInMobile) {
+      document.getElementsByClassName("input")[0].style.display = "block";
       inBox.current.focus();
-    });
-    inBox.current.addEventListener('keydown', (e) => {
-      if (e.keyCode == 13) {
-        if (e.target.value !== "") {
-          processCommand(e.target.value.toString());
-          e.target.value = "";
-          e.preventDefault();
+      inBox.current.addEventListener('blur', (e) => {
+        inBox.current.focus();
+      });
+      inBox.current.addEventListener('keydown', (e) => {
+        if (e.keyCode == 13) {
+          if (e.target.value !== "") {
+            processCommand(e.target.value.toString());
+            e.target.value = "";
+            e.preventDefault();
+          }
         }
-      }
-    });
+      });
+    } else {
+      setIsMobile(true);
+      setTimeout(() => {
+        mobileWarning.current.style.height = window.innerHeight + "px";
+      }, 250);
+    }
   }, []);
 
   return (
     <div className={styles.container}>
-      <span className="output" ref={outputBox} dangerouslySetInnerHTML={{__html: "&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;_&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<br>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;(_)&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<br>__&nbsp;&nbsp;&nbsp;___&nbsp;_&nbsp;__&nbsp;&nbsp;&nbsp;__&nbsp;_&nbsp;&nbsp;__&nbsp;_&nbsp;<br>\\&nbsp;\\&nbsp;/&nbsp;/&nbsp;|&nbsp;'_&nbsp;\\&nbsp;/&nbsp;_`&nbsp;|/&nbsp;_`&nbsp;|<br>&nbsp;\\&nbsp;V&nbsp;/|&nbsp;|&nbsp;|&nbsp;|&nbsp;|&nbsp;(_|&nbsp;|&nbsp;(_|&nbsp;|<br>&nbsp;&nbsp;\\_/&nbsp;|_|_|&nbsp;|_|\\__,&nbsp;|\\__,_|<br>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;__/&nbsp;|&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<br>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;|___/&nbsp;&nbsp;&nbsp;&nbsp;<br>&nbsp;&nbsp;<br>Welcome&nbsp;to&nbsp;my&nbsp;<b>portfolio</b>&nbsp;terminal!<br>Type&nbsp;<b>help</b>&nbsp;to&nbsp;see&nbsp;a&nbsp;list&nbsp;of&nbsp;commands.<br>"}} />
-      <br />
-      <span className="input"><span style={{color: "lime"}}>visitor@vinga.xyz</span>:<span style={{color: "CornflowerBlue"}}>~</span>$ <input style={{width: "calc(100% - 200px)"}} ref={inBox} /></span>
+      {!isMobile ? (
+        <>
+          <span className="output" ref={outputBox} dangerouslySetInnerHTML={{__html: "&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;_&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<br>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;(_)&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<br>__&nbsp;&nbsp;&nbsp;___&nbsp;_&nbsp;__&nbsp;&nbsp;&nbsp;__&nbsp;_&nbsp;&nbsp;__&nbsp;_&nbsp;<br>\\&nbsp;\\&nbsp;/&nbsp;/&nbsp;|&nbsp;'_&nbsp;\\&nbsp;/&nbsp;_`&nbsp;|/&nbsp;_`&nbsp;|<br>&nbsp;\\&nbsp;V&nbsp;/|&nbsp;|&nbsp;|&nbsp;|&nbsp;|&nbsp;(_|&nbsp;|&nbsp;(_|&nbsp;|<br>&nbsp;&nbsp;\\_/&nbsp;|_|_|&nbsp;|_|\\__,&nbsp;|\\__,_|<br>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;__/&nbsp;|&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<br>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;|___/&nbsp;&nbsp;&nbsp;&nbsp;<br>&nbsp;&nbsp;<br>Welcome&nbsp;to&nbsp;my&nbsp;<b>portfolio</b>&nbsp;terminal!<br>Type&nbsp;<b>help</b>&nbsp;to&nbsp;see&nbsp;a&nbsp;list&nbsp;of&nbsp;commands.<br>"}} />
+          <br />
+          <span className="input"><span style={{color: "lime"}}>visitor@vinga.xyz</span>:<span style={{color: "CornflowerBlue"}}>~</span>$ <input style={{width: "calc(100% - 200px)"}} ref={inBox} /></span>
+        </>
+      ) : (
+        <div ref={mobileWarning} style={{display: "flex", justifyContent: "center", alignItems: "center", height: "100%"}}>
+          <span style={{color: "white", width: "100%", textAlign: "center"}}>
+            This experience is best viewed on a computer
+          </span>
+        </div>
+      )}
     </div>
   )
 }
